0dd22fc5f6d0f2714ac6c433b465a81f961326f5
AyushAgrawal <97398126+ayushagrawal07@users.noreply.github.com>
2024-01-26 19:30:41 +0530

    Register and login, Authentication to password

--- client/src/components/user/Login.js
+++ client/src/components/user/Login.js
@@ -1,139 +1,3 @@
-// import {
-//   Button,
-//   Dialog,
-//   DialogActions,
-//   DialogContent,
-//   DialogContentText,
-//   DialogTitle,
-//   IconButton,
-//   TextField,
-// } from "@mui/material";
-// import { Close, Send } from "@mui/icons-material";
-// import { useValue } from "../../context/ContextProvider";
-// import { useEffect, useRef, useState } from "react";
-// import PasswordField from "./PasswordField";
-// import GoogleOneTapLogin from "./GoogleOneTapLogin";
-// const Login = () => {
-//   const {
-//     state: { openLogin },
-//     dispatch,
-//   } = useValue();
-//   const [title, setTitle] = useState("Login");
-//   const [isRegister, setIsRegister] = useState(false);
-//   const nameRef = useRef();
-//   const emailRef = useRef();
-//   const passwordRef = useRef();
-//   const confirmPasswordRef = useRef();
-
-//   const handleClose = () => {
-//     dispatch({ type: "CLOSE_LOGIN" });
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     // testing Loading
-//     dispatch({ type: "START_LOADING" });
-
-//     setTimeout(() => {
-//       dispatch({ type: "END_LOADING" });
-//     }, 6000);
-
-//     const password = passwordRef.current.value;
-//     const confirmPassword = confirmPasswordRef.current.value;
-//     if (password !== confirmPassword) {
-//       dispatch({
-//         type: "UPDATE_ALERT",
-//         payload: {
-//           open: true,
-//           severity: "error",
-//           message: "Passwords do not match",
-//         },
-//       });
-//     }
-//   };
-
-//   useEffect(() => {
-//     isRegister ? setTitle("Register") : setTitle("Login");
-//   }, [isRegister]);
-
-//   return (
-//     <Dialog open={openLogin} onClose={handleClose}>
-//       <DialogTitle>
-//         {title}
-//         <IconButton
-//           sx={{
-//             position: "absolute",
-//             top: 8,
-//             right: 8,
-//             color: (theme) => theme.palette.grey[500],
-//           }}
-//           onClick={handleClose}
-//         >
-//           <Close />
-//         </IconButton>
-//       </DialogTitle>
-//       <form onSubmit={handleSubmit}>
-//         <DialogContent dividers>
-//           <DialogContentText>
-//             Please fill your information in the feilds below:
-//           </DialogContentText>
-//           {isRegister && (
-//             <TextField
-//               autoFocus
-//               margin="normal"
-//               variant="standard"
-//               id="name"
-//               label="Name"
-//               type="text"
-//               fullWidth
-//               inputRef={nameRef}
-//               inputProps={{ minLength: 2 }}
-//               required
-//             />
-//           )}
-//           <TextField
-//             autoFocus={!isRegister}
-//             margin="normal"
-//             variant="standard"
-//             id="email"
-//             label="Email"
-//             type="email"
-//             fullWidth
-//             inputRef={emailRef}
-//             // inputProps={{ minLength: 2 }}
-//             required
-//           />
-//           <PasswordField {...{ passwordRef }} />
-//           {isRegister && (
-//             <PasswordField
-//               passwordRef={confirmPasswordRef}
-//               id="confirmPassword"
-//               label="Confirm Password"
-//             />
-//           )}
-//         </DialogContent>
-//         <DialogActions sx={{ px: "19px" }}>
-//           <Button type="submit" variant="contained" endIcon={<Send />}>
-//             Submit
-//           </Button>
-//         </DialogActions>
-//       </form>
-//       <DialogActions sx={{ justifyContent: "left", p: "5px 24px" }}>
-//         {isRegister
-//           ? "Do you have an account? Sign in now "
-//           : "Don't you have an account? Create one now "}
-//         <Button onClick={() => setIsRegister(!isRegister)}>
-//           {isRegister ? "Login" : "Register"}
-//         </Button>
-//       </DialogActions>
-//       <DialogActions sx={{ justifyContent: "center", py: "24px" }}>
-//         <GoogleOneTapLogin />
-//       </DialogActions>
-//     </Dialog>
-//   );
-// };
-
-// export default Login;
 import { Close, Send } from '@mui/icons-material';
 import {
   Button,
@@ -149,6 +13,7 @@ import { useEffect, useRef, useState } from 'react';
 import { useValue } from '../../context/ContextProvider';
 import GoogleOneTapLogin from './GoogleOneTapLogin';
 import PasswordField from './PasswordField';
+import {register,login} from '../../actions/user';
 
 const Login = () => {
   const {
@@ -168,15 +33,13 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // testing Loading
-    dispatch({ type: 'START_LOADING' });
+    const email=emailRef.current.value;
 
-    setTimeout(() => {
-      dispatch({ type: 'END_LOADING' });
-    }, 6000);
-
-    //testing Notification
+    //testing password
+   
     const password = passwordRef.current.value;
+    if (!isRegister) return login({ email, password }, dispatch);
+    const name = nameRef.current.value;
     const confirmPassword = confirmPasswordRef.current.value;
     if (password !== confirmPassword) {
       dispatch({
@@ -188,6 +51,9 @@ const Login = () => {
         },
       });
     }
+    
+    register({name,email,password},dispatch);
+
   };
 
   useEffect(() => {
--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import roomRouter from './routes/roomRouter.js';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import userRouter from './routes/userRouter.js';
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 const app = express();
+//app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
@@ -21,8 +26,12 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(
+  express.urlencoded({ extended: true })
+);
+ 
 app.use(express.json({ limit: '10mb' }));
-
+app.use('/user', userRouter);
 app.use('/room', roomRouter);
 app.use('/', (req, res) => res.json({ message: 'Welcome to our API' }));
 app.use((req, res) =>
@@ -32,6 +41,8 @@ app.use((req, res) =>
 const startServer = async () => {
   try {
     // connect to mongodb
+    await mongoose.connect(process.env.MongoConnect);
+    console.log("Mongo Db connected")
     app.listen(port, () => console.log(`Server is listening on port: ${port}`));
   } catch (error) {
     console.log(error);
